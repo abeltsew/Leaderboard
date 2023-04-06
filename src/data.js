0ccd93d5075c5fd914964d7a2e3a37422d35440b
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-04-06 21:34:07 +0300

    Add Module for Data

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export default [
+  { name: 'user1', score: 100 },
+  { name: 'user2', score: 200 },
+  { name: 'user3', score: 20 },
+  { name: 'user4', score: 100 },
+  { name: 'user5', score: 30 },
+  { name: 'user6', score: 600 },
+];
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import './style.css';
 
-const data = [
-  { name: 'user1', score: 100 },
-  { name: 'user2', score: 200 },
-  { name: 'user3', score: 20 },
-  { name: 'user4', score: 100 },
-  { name: 'user5', score: 30 },
-  { name: 'user6', score: 600 },
-];
+import data from './data';
 
 const scoreBoard = document.querySelector('.score-table');
 
